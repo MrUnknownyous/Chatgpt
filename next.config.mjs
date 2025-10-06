1dbce10f711df48619d67e15b2ac6f7bf4de0a5f
MrUnknownyous <mr.unknownyous@gmail.com>
2025-10-06 15:27:06 -0400

    fix: configure alias resolution for builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   },
 };
 
